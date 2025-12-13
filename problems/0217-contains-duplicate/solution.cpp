329ad62ec15fa7893ef81c820cb5d231249b91b9
Ronit Kundnani <ronitkundnani08@gmail.com>
2025-12-13 11:39:31 +0000

    Sync LeetCode submission Runtime - 53 ms (37.88%), Memory - 90.8 MB (45.85%)

---
+++ problems/0217-contains-duplicate/solution.cpp
@@ -0,0 +1,10 @@
+class Solution {
+public:
+    bool containsDuplicate(vector<int>& nums) {
+        unordered_set<int> set;
+        for(int x:nums){
+            if(set.count(x)>0) return true;
+            set.insert(x);}
+        return false;
+    }
+};
