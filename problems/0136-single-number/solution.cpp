5574e582a73bf572fa357111aea3db93c21a990c
Ronit Kundnani <ronitkundnani08@gmail.com>
2025-12-13 11:44:52 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 20.8 MB (23.81%)

--- problems/0136-single-number/solution.cpp
+++ problems/0136-single-number/solution.cpp
@@ -1,10 +1,8 @@
 class Solution {
 public:
     int singleNumber(vector<int>& nums) {
-        int result=0;
-        for(int val:nums){
-            result=result^val;
-        }
-        return result;
+        int x=0;
+        for(int num:nums) x^=num;
+        return x;
     }
 };
