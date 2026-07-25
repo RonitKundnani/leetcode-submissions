e6cdc4d3d673a6ab2ed3149db9f82fa5edea1598
Ronit Kundnani <ronitkundnani08@gmail.com>
2026-07-25 18:02:02 +0000

    Sync LeetCode submission Runtime - 4 ms (12.72%), Memory - 17 MB (48.69%)

---
+++ problems/1704-special-positions-in-a-binary-matrix/solution.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    int numSpecial(vector<vector<int>>& mat) {
+        int rows=mat.size();
+        int cols=mat[0].size();
+        vector<int> row(rows,0);
+        vector<int> col(cols,0);
+        for(int i=0;i<rows;i++){
+            for(int j=0;j<cols;j++){
+                if(mat[i][j]==1){
+                    row[i]++;
+                    col[j]++;
+                }
+            }
+        }
+        int ans=0;
+        for(int i=0;i<rows;i++){
+            for(int j=0;j<cols;j++){
+                if(mat[i][j]==1 and row[i]==1 and col[j]==1){
+                    ans++;
+                }
+            }
+        }return ans;
+    }
+};
