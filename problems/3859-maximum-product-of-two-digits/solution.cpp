f1e5e6a382ce2130ae8ff3128739fd48dbb76a41
Ronit Kundnani <ronitkundnani08@gmail.com>
2026-07-25 17:51:40 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 8.7 MB (74.03%)

---
+++ problems/3859-maximum-product-of-two-digits/solution.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int maxProduct(int n) {
+        int first=0;
+        int second=0;
+        while(n){
+            int x=n%10;
+            if(x>=first){
+                second=first;
+                first=x;
+            }else if(x>second) second=x;
+            n=n/10;
+        }
+        return first*second;
+    }
+};
