6f041adadca5942b34100f7a758ccc37c8f47c52
Ronit Kundnani <ronitkundnani08@gmail.com>
2026-07-30 04:18:08 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 25.9 MB (93.91%)

---
+++ problems/0042-trapping-rain-water/solution.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    int trap(vector<int>& height) {
+        int area=0;
+        int left=0;
+        int right=height.size()-1;
+        int leftMax=height[left];
+        int rightMax=height[right];
+        while(left<right){
+            if(leftMax<rightMax){
+                left++;
+                leftMax=max(leftMax,height[left]);
+                area=area+leftMax-height[left];
+            }else{
+                right--;
+                rightMax=max(rightMax,height[right]);
+                area=area+rightMax-height[right];
+            }
+        }return area;
+    }
+};
