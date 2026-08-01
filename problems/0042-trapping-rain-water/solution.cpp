f8287d89c5764fdc73ea26316e77202b87e0cbeb
Ronit Kundnani <ronitkundnani08@gmail.com>
2026-08-01 13:28:37 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 27.3 MB (17.45%)

--- problems/0042-trapping-rain-water/solution.cpp
+++ problems/0042-trapping-rain-water/solution.cpp
@@ -1,21 +1,22 @@
 class Solution {
 public:
     int trap(vector<int>& height) {
+        int n=height.size();
         int area=0;
         int left=0;
-        int right=height.size()-1;
-        int leftMax=height[left];
-        int rightMax=height[right];
-        while(left<right){
-            if(leftMax<rightMax){
-                left++;
-                leftMax=max(leftMax,height[left]);
-                area=area+leftMax-height[left];
-            }else{
-                right--;
-                rightMax=max(rightMax,height[right]);
-                area=area+rightMax-height[right];
-            }
+        int right=n-1;
+        vector<int> leftMax(n,0);
+        leftMax[0]=height[0];
+        vector<int> rightMax(n,0);
+        rightMax[n-1]=height[n-1];
+        for(int i=1;i<int(height.size());i++){
+            leftMax[i]=max(height[i],leftMax[i-1]);
+        }
+        for(int i=n-2;i>=0;i--){
+            rightMax[i]=max(height[i],rightMax[i+1]);
+        }
+        for(int i=0;i<n;i++){
+            area=area+(min(leftMax[i],rightMax[i]))-height[i];
         }return area;
     }
 };
