2e9e58ee5e195f95996a69638c20faf680f1febf
Ronit Kundnani <ronitkundnani08@gmail.com>
2026-08-24 12:05:22 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 17.2 MB (16.45%)

--- problems/2058-concatenation-of-array/solution.cpp
+++ problems/2058-concatenation-of-array/solution.cpp
@@ -1,12 +1,12 @@
 class Solution {
 public:
     vector<int> getConcatenation(vector<int>& nums) {
-        int n=nums.size();
-        vector<int> res(2*n);
-        for(int i=0;i<n;i++){
-            res[i]=nums[i];
-            res[n+i]=nums[i];
+        vector<int> ans;
+        for(int x:nums){
+            ans.push_back(x);
         }
-        return res;
+        for(int x:nums){
+            ans.push_back(x);
+        }return ans;
     }
 };
