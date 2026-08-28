39c5f2f1199b236423d0d14ff5d2283867b68d11
Ronit Kundnani <ronitkundnani08@gmail.com>
2026-08-28 07:55:21 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 8.6 MB (14.27%)

--- problems/0069-sqrtx/solution.cpp
+++ problems/0069-sqrtx/solution.cpp
@@ -1,14 +1,19 @@
 class Solution {
 public:
     int mySqrt(int x) {
-        if(x==0) return 0;
-        if(x<4) return 1;
-        int i;
-        for( i=1;i<=x/2;i++){
-            long long sq=1LL*i*i;
-            if(sq<x) continue;
-            else if(sq==x) return i;
-            else return i-1;
-        }return i-1;
-    }
+        if(x<2) return x;
+        else{
+            int left=1,right=x/2,i,ans=1;
+            while(left<=right){
+                i=left+(right-left)/2;
+                long long sq=1LL*i*i;
+                if(sq<x){
+                    ans=i;
+                    left=i+1;
+                }
+                else if(sq==x) return i;
+                else right=i-1;
+            }return ans;
+        }
+    }   
 };
