89557c97f9c15a3a604e96188dc823ca4fad378d
Ronit Kundnani <ronitkundnani08@gmail.com>
2026-08-28 07:37:20 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 13.5 MB (78.11%)

--- problems/0035-search-insert-position/solution.cpp
+++ problems/0035-search-insert-position/solution.cpp
@@ -1,28 +1,12 @@
 class Solution {
 public:
     int searchInsert(vector<int>& nums, int target) {
-        int f=0;
-        int r=nums.size()-1;
-        int mid;
-        int st=0;
-        while(1){
-            mid=(f+r)/2;
-            if(nums[mid]==st) break;
-            if(nums[mid]==target){
-                return mid;
-            }
-            else if(nums[mid]>target){
-                st=nums[mid];
-                r=mid-1;
-            }
-            else{
-                st=nums[mid];
-                f=mid+1;
-            }
-        }
-        if(target>nums[mid]) return mid+1;
-        else if(target<nums[mid]) return mid;
-        else return 0;
-        return 0;
-    }        
+        int left=0,right=nums.size()-1,mid;
+        while(left<=right){
+            mid=left+(right-left)/2;
+            if(nums[mid]==target){return mid;}
+            else if(nums[mid]>target) right=mid-1;
+            else left=mid+1;
+        }return left;
+    }
 };
