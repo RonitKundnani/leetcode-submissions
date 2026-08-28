73e56b20d612504710211319e8c872c3ce94f9f4
Ronit Kundnani <ronitkundnani08@gmail.com>
2026-08-28 11:30:30 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 18 MB (46.51%)

---
+++ problems/0081-search-in-rotated-sorted-array-ii/solution.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    bool search(vector<int>& nums, int target) {
+        int left=0,right=nums.size()-1,mid;
+        while(left<=right){
+            mid=left+(right-left)/2;
+            if(nums[mid]==target) return true;
+            else if(nums[mid]==nums[left]){
+                left++;
+            }
+            else if(nums[mid]>=nums[left]){
+                if(nums[left]<=target and target<nums[mid])    right=mid-1;
+                else left=mid+1;
+            }else{
+                if(nums[mid]<target and target<=nums[right]) left=mid+1;
+                else    right=mid-1;
+            }
+        }return false;
+    }
+};
