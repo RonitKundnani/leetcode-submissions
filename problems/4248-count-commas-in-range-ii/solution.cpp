8794fb4b9a82f33200783632aff78d3414572f58
Ronit Kundnani <ronitkundnani08@gmail.com>
2026-09-09 06:45:50 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 9 MB (53.45%)

---
+++ problems/4248-count-commas-in-range-ii/solution.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    long long countCommas(long long n) {
+        long long ans = 0;
+        if (n >= 1000) 
+            ans += (n - 1000 + 1) * 1;
+        if (n >= 1000000)
+            ans += (n - 1000000 + 1) * 1;
+        if (n >= 1000000000LL)
+            ans += (n - 1000000000LL + 1) * 1;
+        if (n >= 1000000000000LL)
+            ans += (n - 1000000000000LL + 1) * 1;
+        if (n >= 1000000000000000LL)
+            ans += (n - 1000000000000000LL + 1) * 1;
+        return ans;
+    }
+};
